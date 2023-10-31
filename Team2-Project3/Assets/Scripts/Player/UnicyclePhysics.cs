2f96fccf78734b855d181b90f43f3bfcd7c32e6d
PandasCafe <colby.w.martin28@gmail.com>
2023-10-31 17:31:26 -0400

    Added unicycle to level 1

--- Team2-Project3/Assets/Scripts/Player/UnicyclePhysics.cs
+++ Team2-Project3/Assets/Scripts/Player/UnicyclePhysics.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UnicyclePhysics : MonoBehaviour
 {
     [SerializeField] private GameObject seat;
-    private float rotationPower = 15.0f;
+    private float rotationPower = 50.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class UnicyclePhysics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotationPower = rotationPower + (1f * Time.deltaTime);
+        //rotationPower = rotationPower + (1f * Time.deltaTime);
         transform.Rotate(rotationPower * Time.deltaTime, 0, 0, Space.Self);
         seat.transform.Rotate(-rotationPower * Time.deltaTime, 0, 0, Space.Self);
     }
